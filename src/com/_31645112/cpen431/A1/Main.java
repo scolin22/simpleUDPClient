8e3fc8cd16a50d6abfdfb5587a8c84249e65807c
Colin Stone <colin@bunnylaundering.com>
2016-01-09 20:42:48 -0800

    Added ANT build into JAR artifacts, made CLI friendly.

--- src/com/_31645112/cpen431/A1/Main.java
+++ src/com/_31645112/cpen431/A1/Main.java
@@ -3,6 +3,11 @@ package com._31645112.cpen431.A1;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Invalid number of arguments: e.g. java -jar simpleUDPClient.jar <IP> <port> <ID>");
+            return;
+        }
+
         SecretCodeClient client = new SecretCodeClient(args[0], Integer.parseInt(args[1]));
 
         System.out.println(client.getSecretCode(Integer.parseInt(args[2])));
